33d9c4de1f4f67bb69a59fe569fd9c1845695cc1
Abdul-asa <bigtulahi1@gmail.com>
2026-04-17 23:04:41 +0100

    Bundling

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,11 +1,26 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const shouldAnalyzeBundle = process.env.BUNDLE_ANALYZE === "true";
+
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    shouldAnalyzeBundle &&
+      visualizer({
+        filename: "dist/stats.html",
+        gzipSize: true,
+        brotliSize: true,
+        open: true,
+      }),
+  ],
   server: {
     port: 3000,
   },
